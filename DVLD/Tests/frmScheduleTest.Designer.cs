9d4dbfd0fa33fd484186b9099cf4d4823b129566
Ibrahim Abu-Asaad <i6802275@gmail.com>
2026-09-04 02:20:39 +0300

    Finish frmScheduleTest.

---
+++ DVLD/Tests/frmScheduleTest.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DVLD.Tests
+{
+    partial class frmScheduleTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ctrlSheduleTest1 = new Controls.ctrlSheduleTest();
+            btnClose = new Sunny.UI.UIButton();
+            SuspendLayout();
+            // 
+            // ctrlSheduleTest1
+            // 
+            ctrlSheduleTest1.Font = new Font("Microsoft Sans Serif", 12F);
+            ctrlSheduleTest1.Location = new Point(17, 49);
+            ctrlSheduleTest1.MinimumSize = new Size(1, 1);
+            ctrlSheduleTest1.Name = "ctrlSheduleTest1";
+            ctrlSheduleTest1.RectColor = Color.FromArgb(243, 249, 255);
+            ctrlSheduleTest1.Size = new Size(612, 834);
+            ctrlSheduleTest1.TabIndex = 0;
+            ctrlSheduleTest1.TestTypeID = DVLD_BLL.clsTestType.enTestType.VisionTest;
+            ctrlSheduleTest1.Text = "ctrlSheduleTest1";
+            ctrlSheduleTest1.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            btnClose.Cursor = Cursors.Hand;
+            btnClose.Font = new Font("Microsoft Sans Serif", 12F);
+            btnClose.Location = new Point(264, 889);
+            btnClose.MinimumSize = new Size(1, 1);
+            btnClose.Name = "btnClose";
+            btnClose.Radius = 10;
+            btnClose.Size = new Size(125, 44);
+            btnClose.TabIndex = 42;
+            btnClose.Text = "Close";
+            btnClose.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            btnClose.Click += btnClose_Click;
+            // 
+            // frmScheduleTest
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(648, 942);
+            ControlBox = false;
+            Controls.Add(btnClose);
+            Controls.Add(ctrlSheduleTest1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmScheduleTest";
+            ShowIcon = false;
+            Text = "Schedule Test";
+            ZoomScaleRect = new Rectangle(19, 19, 800, 450);
+            Load += frmScheduleTest_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Controls.ctrlSheduleTest ctrlSheduleTest1;
+        private Sunny.UI.UIButton btnClose;
+    }
+}
---
+++ DVLD/Tests/frmScheduleTest.cs
@@ -0,0 +1,42 @@
+﻿using DVLD_BLL;
+using Sunny.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Tests
+{
+    public partial class frmScheduleTest : UIForm
+    {
+
+        private int _LDLAppID = -1;
+        private clsTestType.enTestType _TestType = clsTestType.enTestType.VisionTest;
+        private int _TestAppointmentID = -1;
+
+        public frmScheduleTest(int LDLAppID, clsTestType.enTestType TestType, int TestAppointmentID = -1)
+        {
+            InitializeComponent();
+
+            _LDLAppID = LDLAppID;
+            _TestType = TestType;
+            _TestAppointmentID = TestAppointmentID;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmScheduleTest_Load(object sender, EventArgs e)
+        {
+            ctrlSheduleTest1.TestTypeID = _TestType;
+            ctrlSheduleTest1.LoadInfo(_LDLAppID, _TestAppointmentID);
+        }
+    }
+}
