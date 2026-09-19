6a337c4cf7635da81688cb167d874120074abda4
Ibrahim Abu-Asaad <i6802275@gmail.com>
2026-09-19 14:51:25 +0300

    Initialize (DAL + BLL) of License class.

---
+++ DVLD_BLL/clsLicense.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BLL
+{
+    public class clsLicense
+    {
+
+        public enum enMode { AddNew,Update };
+        public enMode Mode = enMode.AddNew;
+
+        public enum enIssueReason { FirstTime, Renew, DamagedReplacement, LostReplacement};
+
+        public int ID { set; get; }
+        public int ApplicationID { set; get; }
+        public int DriverID { set; get; }
+        public clsDriver DriverInfo;
+
+        public int LicenseClass { set; get; }
+        public clsLicenseClass LicenseClassInfo;
+        public DateTime IssueDate { set; get; }
+        public DateTime ExpirationDate { set; get; }
+        public string Notes { set; get; }
+        public float PaidFees { set; get; }
+        public bool IsActive { set; get; }
+        public enIssueReason IssueReason { set; get; }
+        public int CreatedByUserID { set; get; }
+
+
+
+    }
+}
---
+++ DVLD_DAL/clsDataLicense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DAL
+{
+    public class clsDataLicense
+    {
+
+        readonly static string ConnectionString = clsDataAccessSettings.ConnectionString;
+        SqlConnection connection = new SqlConnection(ConnectionString);
+
+
+
+
+
+
+    }
+}
